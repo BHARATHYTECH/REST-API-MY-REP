700fa0232f21156c5a807a10f4f76d6db4ff9fd1
Tech@plan <108079289+BHARATHYTECH@users.noreply.github.com>
2022-06-29 23:04:51 +0530

    initial test

---
+++ Entities/Product.cs
@@ -0,0 +1,12 @@
+namespace WebApi.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+     public string Designation { get; set; }  
+     
+        public string Address { get; set; } 
+
+}
